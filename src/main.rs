14df941c48416843105c82655615409b3700078f
sharmuz <muzher@gmail.com>
2026-05-10 16:47:24 +0100

    Implement basic token parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let lisp_expr = "(first (list 1 (+ 2 3) 9))";
-    let tokens = tokenize(lisp_expr);
-    println!("{tokens:?}");
+    let input = "(first (list 1 (+ 2 3) 9))";
+    println!("Input is: {input}");
+    let tokens = tokenize(input);
+    println!("Tokens are: {tokens:?}");
+    let expr = Expr::from(tokens);
+    println!("Expression is: {expr:?}");
 }
 
 fn tokenize(input: &str) -> Vec<Token> {
@@ -33,6 +36,87 @@ fn tokenize(input: &str) -> Vec<Token> {
 
     tokens
 }
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Expr {
+    Atom(Atom),
+    List(List),
+}
+
+#[allow(clippy::fallible_impl_from)]
+impl From<Vec<Token>> for Expr {
+    fn from(tokens: Vec<Token>) -> Self {
+        match &tokens[0] {
+            Token::Atom(a) => {
+                if tokens.len() > 1 {
+                    panic!("Invalid expr: atom must occur alone")
+                } else {
+                    Self::Atom(a.clone())
+                }
+            }
+            Token::OpenBracket => {
+                assert!(
+                    tokens.last() == Some(&Token::CloseBracket),
+                    "Invalid expr: missing final closing bracket"
+                );
+                let op = if let Token::Atom(a) = &tokens[1] {
+                    a.clone()
+                } else {
+                    panic!("Invalid expr: form is missing operator")
+                };
+
+                let mut args: Vec<Self> = Vec::new();
+                let mut partial_arg: Vec<Token> = Vec::new();
+                let mut num_open_brackets = 0;
+
+                for t in tokens.iter().skip(2).take(tokens.len() - 3) {
+                    match t {
+                        Token::Atom(a) => {
+                            if num_open_brackets == 0 {
+                                args.push(Self::Atom(a.clone()));
+                            } else {
+                                partial_arg.push(t.clone());
+                            }
+                        }
+                        Token::OpenBracket => {
+                            num_open_brackets += 1;
+                            partial_arg.push(t.clone());
+                        }
+                        Token::CloseBracket => {
+                            num_open_brackets -= 1;
+                            partial_arg.push(t.clone());
+
+                            if num_open_brackets == 0 {
+                                let next_arg = Self::from(partial_arg.clone());
+                                args.push(next_arg);
+                                partial_arg.clear();
+                            }
+                        }
+                    }
+                }
+                Self::List(List::Form(Form { operator: op, args }))
+            }
+            Token::CloseBracket => {
+                panic!("Invalid expr: cannot start with closing bracket")
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum List {
+    Form(Form),
+    Data(Data),
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Form {
+    operator: Atom,
+    args: Vec<Expr>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Data(Vec<Expr>);
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Token {
@@ -121,6 +205,42 @@ mod test {
         let tokens = tokenize(input);
 
         assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn expr_from_nested_forms() {
+        let tokens = vec![
+            Token::OpenBracket,
+            Token::Atom(Atom::Symbol("first".to_string())),
+            Token::OpenBracket,
+            Token::Atom(Atom::Symbol("list".to_string())),
+            Token::Atom(Atom::Number(1)),
+            Token::OpenBracket,
+            Token::Atom(Atom::Symbol("+".to_string())),
+            Token::Atom(Atom::Number(2)),
+            Token::Atom(Atom::Number(3)),
+            Token::CloseBracket,
+            Token::Atom(Atom::Number(9)),
+            Token::CloseBracket,
+            Token::CloseBracket,
+        ];
+        let expected = Expr::List(List::Form(Form {
+            operator: Atom::Symbol("first".to_string()),
+            args: vec![Expr::List(List::Form(Form {
+                operator: Atom::Symbol("list".to_string()),
+                args: vec![
+                    Expr::Atom(Atom::Number(1)),
+                    Expr::List(List::Form(Form {
+                        operator: Atom::Symbol("+".to_string()),
+                        args: vec![Expr::Atom(Atom::Number(2)), Expr::Atom(Atom::Number(3))],
+                    })),
+                    Expr::Atom(Atom::Number(9)),
+                ],
+            }))],
+        }));
+        let expr = Expr::from(tokens);
+
+        assert_eq!(expr, expected);
     }
 
     #[test]
